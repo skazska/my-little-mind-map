6c0fbd65d9449a5efc364998d581150ed523515e
Said Karimov <skazska@gmail.com>
2026-04-19 07:20:15 +0300

    Add upload and filesystem plugins to Tauri application

--- desktop-app/src-tauri/src/lib.rs
+++ desktop-app/src-tauri/src/lib.rs
@@ -609,6 +609,8 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_upload::init())
+        .plugin(tauri_plugin_fs::init())
         .setup(|app| {
             let data_dir = app
                 .path()
